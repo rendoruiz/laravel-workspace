ca7612988b37f2a69f501b0ffc5e5bd7f7013375
rendoruiz <rendo.ruiz@outlook.com>
2021-10-02 18:57:40 -0600

    #18 webform setup

--- the net ninja tutorial/pizzahouse/resources/views/pizzas/create.blade.php
+++ the net ninja tutorial/pizzahouse/resources/views/pizzas/create.blade.php
@@ -1,12 +1,26 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-  <div class="content">
-    <div class="title m-b-md">
-      Add a New Pizza
-    </div>
-
-  </div>
+<div class="wrapper create-pizza">
+  <h1>Create a New Pizza</h1>
+  <form action="/pizzas" method="POST">
+    <label for="name">Your name:</label>
+    <input type="text" id="name" name="name">
+    <label for="type">Choose pizza type:</label>
+    <select name="type" id="type">
+      <option value="margherita"> Margherita</option>
+      <option value="hawaiian">Hawaiian</option>
+      <option value="veg supreme">Veg Supreme</option>
+      <option value="volcano">Volcano</option>
+    </select>
+    <label for="base">Choose base type:</label>
+    <select name="base" id="base">
+      <option value="cheesy crust">Cheesy Crust</option>
+      <option value="garlic crust">Crust</option>
+      <option value="this & crispy">Thin & crispy</option>
+      <option value="thick">Thich</option>
+    </select>
+    <input type="submit" value="Order Pizza">
+  </form>
 </div>
 @endsection

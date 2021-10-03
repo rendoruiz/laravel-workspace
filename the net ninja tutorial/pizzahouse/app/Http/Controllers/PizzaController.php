67b31ccebee368726614b01c24f9bec7376aa1f9
rendoruiz <rendo.ruiz@outlook.com>
2021-10-02 19:12:34 -0600

    #20 saving records & redirect session variables

--- the net ninja tutorial/pizzahouse/app/Http/Controllers/PizzaController.php
+++ the net ninja tutorial/pizzahouse/app/Http/Controllers/PizzaController.php
@@ -30,9 +30,15 @@ class PizzaController extends Controller
 	}
 
 	public function store() {
-		error_log(request('name'));
-		error_log(request('type'));
-		error_log(request('base'));
-		return redirect('/');
+		$pizza = new Pizza();
+
+		$pizza->name = request('name');
+		$pizza->type = request('type');
+		$pizza->base = request('base');
+
+		// error_log($pizza);
+		$pizza->save();
+
+		return redirect('/')->with('msg', 'Thanks for your order!');
 	}
 }
